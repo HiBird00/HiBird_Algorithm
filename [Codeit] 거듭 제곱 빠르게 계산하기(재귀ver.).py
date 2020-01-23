0316dd7b84ef198f43d13211f9ce476e8d2020a6
HiBird00 <48594896+HiBird00@users.noreply.github.com>
2020-01-23 20:31:24 +0900

    Update [Codeit] 거듭 제곱 빠르게 계산하기(재귀ver.).py

--- [Codeit] 거듭 제곱 빠르게 계산하기(재귀ver.).py
+++ [Codeit] 거듭 제곱 빠르게 계산하기(재귀ver.).py
@@ -1,9 +1,14 @@
+# 시간 복잡도 : lg(y)
 def power(x, y):
     # 코드를 작성하세요.
     if y == 1:
         return x
     
-    return x * power(x, y-1)
+    # y가 홀수
+    if y % 2 != 0:
+        return x * power(x, y // 2) * power(x, y // 2)
+    
+    return power(x, y // 2) * power(x, y // 2)
 
 
 # 테스트
