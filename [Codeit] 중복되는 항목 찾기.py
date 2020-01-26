b837dbdd3a0e3eb7e1d202907fe73a5f7459c52e
HiBird00 <48594896+HiBird00@users.noreply.github.com>
2020-01-26 22:17:25 +0900

    Update [Codeit] 중복되는 항목 찾기.py

--- [Codeit] 중복되는 항목 찾기.py
+++ [Codeit] 중복되는 항목 찾기.py
@@ -13,3 +13,27 @@ def find_same_number(some_list):
 print(find_same_number([1, 4, 3, 5, 3, 2]))
 print(find_same_number([4, 1, 5, 2, 3, 5]))
 print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
+
+#------------------------------------------------------------------------------#
+
+# 코드잇 해설
+# 시간 복잡도 : O(n)
+def find_same_number(some_list):
+    # 코드를 쓰세요
+    # 확인한 요소를 저장하는 사전
+    elements_seen_so_far = {}
+
+    for element in some_list:
+        # 이미 나온 요소인지 확인하고 맞으면 요소를 리턴한다
+        if element in elements_seen_so_far:
+            return element
+
+        # 해당 요소를 사전에 저장시킨다
+        elements_seen_so_far[element] = True
+        
+
+# 중복되는 수 ‘하나’만 리턴합니다.
+print(find_same_number([1, 4, 3, 5, 3, 2]))
+print(find_same_number([4, 1, 5, 2, 3, 5]))
+print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
+
